0fbd98a822fe0099cd8e09eac28e92a76e013608
Sebastian Drewes <sebas.drewes@gmail.com>
2021-12-11 23:44:39 -0300

    refactored api method

--- src/api/index.js
+++ src/api/index.js
@@ -1,10 +1,10 @@
-export default function activitiesData() {
-  fetch("https://json-biglifeapp.herokuapp.com/activity?_page=1&_limit=6").then(
-    (data) => {
-      data.json().then((products) => {
-        console.log(products);
-        return products;
-      });
-    }
+export default async function getActivitiesData() {
+  const activityData = await fetch(
+    "https://json-biglifeapp.herokuapp.com/activity?_page=1&_limit=6"
   );
+  const jsonActivityData = await activityData.json();
+  const parsedActivityData = jsonActivityData.map((activity) =>
+    JSON.parse(activity.activity)
+  );
+  return parsedActivityData;
 }
