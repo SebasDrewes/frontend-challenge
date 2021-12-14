471f326ddaa25d46e466a40c0d8bbbaae75326d3
Sebastian Drewes <sebas.drewes@gmail.com>
2021-12-14 20:21:44 -0300

    started pagination component

---
+++ src/components/Pagination.vue
@@ -0,0 +1,25 @@
+<template>
+  <ul>
+    <li>
+      <button>First</button>
+    </li>
+    <li>
+      <button>Previous</button>
+    </li>
+    <li>
+      <button>Next</button>
+    </li>
+    <li>
+      <button>Last</button>
+    </li>
+  </ul>
+</template>
+
+<script setup>
+const props = defineProps([
+  "maxVisibleButtons",
+  "totalPages",
+  "perPage",
+  "currentPage",
+]);
+</script>
