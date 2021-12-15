c29f7a99d0316b43b7a4aff0ae8c11183654bd82
Sebastian Drewes <sebas.drewes@gmail.com>
2021-12-15 12:39:02 -0300

    added slider for similar activities

--- src/components/SimilarActivities.vue
+++ src/components/SimilarActivities.vue
@@ -1,27 +1,45 @@
 <template>
   <div>
     <h1 class="similarActivitiesTitle">Otras actividades similares:</h1>
-    <div class="similarActivitiesContainer">
-      <ActivityItem
-        v-for="activity in similarActivitesData"
-        :key="activity.id"
-        :activity="activity"
-        :small="true"
-      />
-    </div>
+    <vueper-slides
+      class="similarActivitiesCarousel no-shadow"
+      fade
+      :visible-slides="4"
+      fixed-height="479px"
+      :bullets="false"
+      slide-multiple
+      :gap="2"
+      disable-arrows-on-edges
+    >
+      <template #arrow-left>
+        <img src="@/assets/blackLeftArrow.svg" class="blackArrow" />
+      </template>
+      <template #arrow-right>
+        <img src="@/assets/blackRightArrow.svg" class="blackArrow" />
+      </template>
+      <vueper-slide v-for="activity in similarActivitesData" :key="activity.id">
+        <template v-slot:content>
+          <ActivityItem :activity="activity" :small="true" />
+        </template>
+      </vueper-slide>
+    </vueper-slides>
   </div>
 </template>
 
 <script setup>
 import ActivityItem from "./ActivityItem.vue";
+import { VueperSlides, VueperSlide } from "vueperslides";
+import "vueperslides/dist/vueperslides.css";
 import { useRoute } from "vue-router";
 import { ref, onMounted, watch } from "vue";
 import { randomActivities } from "@/api";
 const route = useRoute();
 const similarActivitesData = ref([]);
+
 async function fetchActivityData() {
-  const activityData = await randomActivities(4);
+  const activityData = await randomActivities(8);
   similarActivitesData.value = activityData;
+  console.log(activityData);
 }
 onMounted(fetchActivityData());
 watch(
@@ -43,5 +61,11 @@ watch(
   line-height: 30px;
   letter-spacing: -1px;
   margin: 64px 0 30px;
+}
+.similarActivitiesCarousel {
+  width: 100%;
+}
+.blackArrow {
+  fill: black;
 }
 </style>
