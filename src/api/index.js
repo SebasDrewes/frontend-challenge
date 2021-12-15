ec86925b4ebb04ea2733613e4883dd314dcae75d
Sebastian Drewes <sebas.drewes@gmail.com>
2021-12-14 21:53:47 -0300

    refactor method to take page and limit

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
-export function getActivitiesData() {
+export function getActivitiesData(page, limit) {
   return fetch(
-    "https://json-biglifeapp.herokuapp.com/activity?_page=1&_limit=6"
+    `https://json-biglifeapp.herokuapp.com/activity?_page=${page}&_limit=${limit}`
   )
     .then((activities) => activities.json())
     .then((activities) =>
@@ -11,18 +11,6 @@ export function getActivity(id) {
   return fetch(`https://json-biglifeapp.herokuapp.com/activity/${id}`)
     .then((activity) => activity.json())
     .then((activity) => JSON.parse(activity.activity));
-}
-export function similarActivities(category, amount) {
-  return fetch("https://json-biglifeapp.herokuapp.com/activity")
-    .then((activities) => activities.json())
-    .then((activities) =>
-      activities.map((activity) => JSON.parse(activity.activity))
-    )
-    .then((activities) =>
-      activities.filter((activity) => activity.category === category)
-    )
-    .then((activities) => activities.sort((a, b) => 0.5 - Math.random()))
-    .then((activities) => activities.slice(0, amount));
 }
 export function randomActivities(amount) {
   const randomPage = Math.random() * (200 - 1) + 1;
